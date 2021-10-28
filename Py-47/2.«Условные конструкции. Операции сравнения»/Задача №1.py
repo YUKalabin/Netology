34afb029e7c909367a8609b8604fe07db4f20f73
Калабин <yukalabin@gmail.com>
2021-10-28 12:16:01 +0300

    gitignore

--- Py-47/2.«Условные конструкции. Операции сравнения»/Задача №1.py
+++ Py-47/2.«Условные конструкции. Операции сравнения»/Задача №1.py
@@ -1,9 +1,17 @@
-n=int(input("1 <= n <= 100"))
+
+if __name__ == '__main__':
+    n = int(raw_input().strip())
+
+
+    n = int(input())
 if n % 2 == 1:
     print("Weird")
 elif 2 <= n <= 5:
     print("Not Weird")
 elif 6 <= n <= 20:
     print("Weird")
-elif n > 20:
+elif 20 < n <= 100:
     print("Not Weird")
+else:
+    print("Error")
+    
